399d07ffcdf171e90c322b02f1b4082b520b3dee
boxpenguin <>
2023-04-11 19:35:59 -0400

    troubleshooting Path

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def enable_url():
 #     return {"message": test.toggle(is_enabled=False)}
 
 @app.get("/disable/{adlist_comment}")
-async def disable_url(adlist_comment: str = Path("scheduled", description="The adlist comment you wish to access.")):
+async def disable_url(adlist_comment: str = Path(None, description="The adlist comment you wish to access.")):
     test = GravityDatabase(comment=adlist_comment)
     return {"message": test.toggle(is_enabled=False)}
 
