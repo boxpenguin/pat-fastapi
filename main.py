b5ba34d928af21243139abf4d3bd7b7ecbfaef14
boxpenguin <>
2023-04-19 20:08:24 -0400

    trying to get old response

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/update", response_class=PlainTextResponse)
+@app.get("/update")#, response_class=PlainTextResponse)
 def update_url():
     test = GravityDatabase()
     output = test.update()
