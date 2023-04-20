4328f43d11ecdb58a2911fefa8566c9067dada51
boxpenguin <>
2023-04-19 23:02:37 -0400

    trying to add some httpexceptions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import PlainTextResponse
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import PlainTextResponse, RedirectResponse
 from gravitydatabase import GravityDatabase
 from pydantic import BaseModel, HttpUrl
 
@@ -8,26 +8,32 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    return RedirectResponse("/pat-fastapi/")
+
+@app.get("/pat-fastapi/")
+async def root():
     return {"message": "Hello World"}
 
-@app.get("/update", response_class=PlainTextResponse)
+@app.get("/pat-fastapi/update", response_class=PlainTextResponse)
 def update_url():
     test = GravityDatabase()
     output = test.update()
     return output.replace("\u001b[K", "")
 
-@app.get("/enable/{adlist_comment}")
+@app.get("/pat-fastapi/enable/{adlist_comment}")
 async def enable_url(adlist_comment: str):
     test = GravityDatabase(comment=adlist_comment)
     return {"message": test.enable_adlist()}
-    # add proper documentation and status codes from 
 
-@app.get("/disable/{adlist_comment}")
+@app.get("/pat-fastapi/disable/{adlist_comment}")
 async def disable_url(adlist_comment: str):
     test = GravityDatabase(comment=adlist_comment)
     return {"message": test.disable_adlist()}
 
-@app.get("/status/{adlist_comment}")
+@app.get("/pat-fastapi/status/{adlist_comment}")
 async def status(adlist_comment: str):
     test = GravityDatabase(comment=adlist_comment)
-    return {"message": test.status()}
+    result = test.status()
+    if result is None:
+        raise HTTPException(status_code=404, detail=f"Adlist with comment '{comment}' not found.")
+    return {"message": result}
